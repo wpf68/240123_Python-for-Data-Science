6211fcfb930329b23309e13262fd5c3b4d4ce4d9
Pascal Wolff <pwolff@k2r3p8.42mulhouse.fr>
2024-02-09 17:56:08 +0100

    240209 ex05 en cours plus flak8

--- Python_for_data_science/Python_0_Starting/ex05/building.py
+++ Python_for_data_science/Python_0_Starting/ex05/building.py
@@ -1,30 +1,44 @@
-# **************************************************************************** #
-#                                                                              #
-#                                                         :::      ::::::::    #
-#    building.py                                        :+:      :+:    :+:    #
-#                                                     +:+ +:+         +:+      #
-#    By: pwolff <pwolff@student.42.fr>              +#+  +:+       +#+         #
-#                                                 +#+#+#+#+#+   +#+            #
-#    Created: 2024/02/08 08:20:39 by pwolff            #+#    #+#              #
-#    Updated: 2024/02/08 09:28:32 by pwolff           ###   ########.fr        #
-#                                                                              #
-# **************************************************************************** #
+# ****************************************************************************#
+#                                                                             #
+#                                                         :::      ::::::::   #
+#    building.py                                        :+:      :+:    :+:   #
+#                                                     +:+ +:+         +:+     #
+#    By: pwolff <pwolff@student.42.fr>              +#+  +:+       +#+        #
+#                                                 +#+#+#+#+#+   +#+           #
+#    Created: 2024/02/08 08:20:39 by pwolff            #+#    #+#             #
+#    Updated: 2024/02/09 17:34:30 by pwolff           ###   ########.fr       #
+#                                                                             #
+# ****************************************************************************#
+
+"""
+    Chemin pour flack8 :
+    export PATH=$PATH:/home/pwolff/.local/bin
+
+    lire la doc :
+    python3
+    import building
+    help(building)
+"""
 
 import sys
 # import string
 
-def main():
-    tab = sys.argv
-    strPunctuation = "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~"
 
-    nbUpperCase = sum(1 for value in tab[1] if value.isupper())
-    nbLowerCase = sum(1 for value in tab[1] if value.islower())
-    nbPunctuation = sum(1 for value in tab[1] if value in strPunctuation)
-    nbDigits = sum(1 for value in tab[1] if value.isdigit())
-    nbSpaces = sum(1 for value in tab[1] if value.isspace() or value == '\n')
+def treatement(sentence):
+    """
+        Traitement de la String
+
+        Nombre de punctuations : strPunctuation
+
+    """
+    strPunctuation = "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~"
+    nbUpperCase = sum(1 for value in sentence if value.isupper())
+    nbLowerCase = sum(1 for value in sentence if value.islower())
+    nbPunctuation = sum(1 for value in sentence if value in strPunctuation)
+    nbDigits = sum(1 for value in sentence if value.isdigit())
+    nbSpaces = sum(1 for value in sentence if value.isspace() or value == '\n')
 
     # strPunctuation = string.punctuation
-    
     # for value in tab[1]:
     #     if value.isupper():
     #         nbUpperCase += 1
@@ -37,12 +51,38 @@ def main():
     #     elif value == " " or value =="\n":
     #         nbSpaces += 1
 
-    print(f"The text contains {len(tab[1])} characters:")
+    print(f"The text contains {len(sentence)} characters:")
     print(f"{nbUpperCase} upper letters")
     print(f"{nbLowerCase} lower letters")
     print(f"{nbPunctuation} punctuation marks")
     print(f"{nbSpaces} spaces")
     print(f"{nbDigits} digits")
+
+
+def main():
+    """
+        Test nombre d'arguments
+    
+    
+    """
+    tab = sys.argv
+
+    try:
+        if len(tab) > 2:
+            raise AssertionError("Error too many arguments !!!")
+        while True:
+            if len(tab) == 2:
+                sentence = tab[1]
+                break
+            sentence = input("Which sentence do you want to count ? \n")
+            if sentence:
+                break
+
+    except AssertionError as error:
+        print(AssertionError.__name__+": ", error)
+        sys.exit()
+
+    treatement(sentence)
 
 
 if __name__ == "__main__":
---
+++ Python_for_data_science/testFlack8/testFlack8.py
@@ -0,0 +1,23 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    testFlack8.py                                      :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: pwolff <pwolff@student.42.fr>              +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2024/02/09 17:12:36 by pwolff            #+#    #+#              #
+#    Updated: 2024/02/09 17:12:57 by pwolff           ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import mylib  # noqa (i.e ignore cette erreur)
+
+def foo( bar):
+    print 'foo {}'.format(bar)
+
+
+    
