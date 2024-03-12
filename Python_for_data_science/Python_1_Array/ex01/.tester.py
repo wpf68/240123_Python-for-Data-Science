9962dfeb84778f66d85d3ab7db6e304d0dcece6a
Pascal Wolff <pwolff@k0r4p14.42mulhouse.fr>
2024-03-12 08:07:33 +0100

    240312 01-01

--- Python_for_data_science/Python_1_Array/ex01/.tester.py
+++ Python_for_data_science/Python_1_Array/ex01/.tester.py
@@ -13,9 +13,28 @@
 from array2D import slice_me
 
 family = [[1.80, 78.4],
-[2.15, 102.7],
-[2.10, 98.5],
-[1.88, 75.2]]
+          [2.15, 102.7],
+          [2.10, 98.5],
+          [1.88, 75.2]]
 print(slice_me(family, 0, 2))
 print(slice_me(family, 1, -2))
 
+print("\n********** Test list empty **********")
+family = []
+print(slice_me(family, 1, -2))
+
+print("\n********** Test No list  **********")
+family = 1
+print(slice_me(family, 1, -2))
+
+print("\n********** Test list no valid for numpy  **********")
+family = [[1, 2, 3], [4, 5]]
+print(slice_me(family, 1, -2))
+
+print("\n********** Test slicing no valid  **********")
+family = [[1, 2, 3], [4, 5, 8]]
+print(slice_me(family, 100000000000000000000000000000000000, -2))
+
+print("\n********** Test slicing no valid  **********")
+family = [[1, 2, 3], [4, 5, 8]]
+print(slice_me(family, "hello", -2))
--- Python_for_data_science/Python_1_Array/ex01/array2D.py
+++ Python_for_data_science/Python_1_Array/ex01/array2D.py
@@ -23,24 +23,70 @@ Chemin pour flack8 :
 export PATH=$PATH:/home/pwolff/.local/bin
 """
 
-# import numpy as np
+import numpy as np
+
 
 def slice_me(family: list, start: int, end: int) -> list:
     """
-    
+    Write a function that takes as parameters a 2D array, prints its shape,
+    and returns a truncated version of the array based on the provided start
+    and end arguments.
+    You must use the slicing method.
+    You have to handle error cases if the lists are not the same size,
+    are not a list ...
+
+    Args:
+        family (list): 2D array to be truncated.
+        start (int): Index to start truncation from.
+        end (int): Index to end truncation at.
+
+    Returns:
+        list: Truncated version of the array.
+    My shape:
+        Calculates the shape of the original family array
+        using NumPy's np.array() function and then retrieves its
+        shape using the .shape attribute. The shape of a 2D array is
+        a tuple representing the number of rows and columns.
+        This line prints the shape of the original array.
+    New shape:
+        Calculates the shape of the truncated array.
+        It first creates a NumPy array from the family list,
+        then slices it from the start index to the end+1 index.
+        The .shape attribute is used to retrieve the shape of the
+        truncated array, which represents the number of rows and
+        columns after the truncation.
     """
-    # source = np.array(family)
-    # print(source)
-    # print(type(source))
-    # result = source[start:end]
-    result = family[start:end]
 
-    # print(source[start:end])
-    print(f"My shape is : ({len(family)}, {len(family[0])})")
-    print(f"My new shape is : ({len(result)}, {len(result[0])})")
+    # Method Python **********************************************
+    # result = family[start:end]
+    # print(f"My shape is : ({len(family)}, {len(family[0])})")
+    # print(f"My new shape is : ({len(result)}, {len(result[0])})")
+    # ************************************************************
 
-    return result
+    # Method numpy ***********************************************
 
+    try:
+        if not isinstance(family, list) or not isinstance(start, int) \
+                or not isinstance(end, int):
+            raise AssertionError("Value not valid !!!")
+    except AssertionError as error:
+        print("\033[1;31m" + AssertionError.__name__ +
+              " : " + str(error) + "\033[1;37m")
+        return list()
+
+    try:
+        source = np.array(family)
+    except Exception:
+        print("\033[1;31mError size list" + "\033[1;37m")
+        return list()
+
+    result = source[start: end]
+    print(f"My shape is : {source.shape}")
+    print(f"My new shape is : {result.shape}")
+
+    # ************************************************************
+
+    return result.tolist()
 
 
 def main():
@@ -51,4 +97,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
