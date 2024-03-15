1818534ccfda8bac2795db28d2c9d67deb72c212
Pascal Wolff <pwolff@k0r4p14.42mulhouse.fr>
2024-03-15 08:48:23 +0100

    240315 01-02

---
+++ Python_for_data_science/Python_1_Array/ex02/.tester.py
@@ -0,0 +1,19 @@
+# ****************************************************************************#
+#                                                                             #
+#                                                         :::      ::::::::   #
+#    .tester.py                                         :+:      :+:    :+:   #
+#                                                     +:+ +:+         +:+     #
+#    By: pwolff <pwolff@student.42.fr>              +#+  +:+       +#+        #
+#                                                 +#+#+#+#+#+   +#+           #
+#    Created: 2024/03/15 06:52:15 by pwolff            #+#    #+#             #
+#    Updated: 2024/03/15 06:52:15 by pwolff           ###   ########.fr       #
+#                                                                             #
+# ****************************************************************************#
+
+
+from load_image import ft_load
+
+print(ft_load("landscape.jpg"))
+print(ft_load("animal.jpeg"))
+print(ft_load("error.jpeg"))
+print(ft_load("animal.png"))
