e84d9c1230d9a2c70b3c92b3493213c16b628ffe
Pascal Wolff <pwolff@k0r4p14.42mulhouse.fr>
2024-03-16 08:11:21 +0100

    240316 01-03

---
+++ Python_for_data_science/Python_1_Array/ex03/zoom.py
@@ -0,0 +1,25 @@
+# ****************************************************************************#
+#                                                                             #
+#                                                         :::      ::::::::   #
+#    zoom.py                                            :+:      :+:    :+:   #
+#                                                     +:+ +:+         +:+     #
+#    By: pwolff <pwolff@student.42.fr>              +#+  +:+       +#+        #
+#                                                 +#+#+#+#+#+   +#+           #
+#    Created: 2024/03/16 07:01:22 by pwolff            #+#    #+#             #
+#    Updated: 2024/03/16 07:01:22 by pwolff           ###   ########.fr       #
+#                                                                             #
+# ****************************************************************************#
+
+
+from load_image import ft_load
+
+"""
+https://infoforall.fr/python/python-act040.html
+
+"""
+
+def zoom():
+    print(ft_load("animal.jpeg"))
+
+if __name__ == "__main__":
+    zoom()
