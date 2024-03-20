0267b5d611cbe7d48839b8054d59e8c7ab483950
Pascal Wolff <pwolff@k0r4p14.42mulhouse.fr>
2024-03-20 09:05:07 +0100

    240320 01-04 OK

--- Python_for_data_science/Python_1_Array/ex04/rotate.py
+++ Python_for_data_science/Python_1_Array/ex04/rotate.py
@@ -18,7 +18,20 @@ import matplotlib.pyplot as plt
 
 def rotate():
     try:
-        image = (ft_load("ImageNB.jpeg"))
+        typeImage = int((input("Image square(1) or rectangle(2) ?")) or '1')
+        print(typeImage)
+    except ValueError as error:
+        print(f"\033[1;31m{ValueError.__name__} : {error}\033[1;37m")
+        exit()
+    try:
+        if typeImage == 1:
+            image = (ft_load("ImageNB.jpeg"))
+        else:
+            image = (ft_load("landscape.jpg"))
+            image = Image.fromarray(image)
+            image = image.convert("L")
+            image = np.array(image)
+
         if isinstance(image, str) and image == "":
             raise AssertionError("Error ft load_image !!!")
         else:
@@ -27,46 +40,57 @@ def rotate():
         print(f"\033[1;31m{AssertionError.__name__} : {error}\033[1;37m")
         exit()
 
+    print(image.shape)
     y, x = image.shape
-    
-    imageTemp = np.empty((y,x))
+    print(f"y : {y}  --  x : {x}")
 
-    #******************** REVERSE *****************************
+    imageTemp = np.empty((y, x))
+    print(imageTemp.shape)
+
+# ******************** REVERSE *****************************
     i = 0
     while i < y:
-        temp = image[i:i+1,0:(x + 1)]
+        temp = image[i]
         # print(f"{i} : {temp}")
-        # imageTemp[i] = np.append(imageTemp[i], temp)
         imageTemp[(y - 1) - i] = np.array(temp)
         i += 1
 
-    print(f"\033[1;32mThe shape of image is: {imageTemp.shape}\033[1;37m")
+    print(f"\033[1;32mNew shape after Transpose (Reverse): \
+{imageTemp.shape}\033[1;37m")
     print(imageTemp)
     imageReverse = Image.fromarray(imageTemp)
+# **********************************************************
 
-    #**********************************************************
-
-    #******************** Rotate *****************************
+# ******************** Rotate *****************************
+    imageTemp = np.empty((x, y))
     i = 0
     while i < y:
-        temp = image[i:i+1,0:(x+1)]
-        # print(f"{i} : {temp}")
-        # imageTemp[i] = np.append(imageTemp[i], temp)
-        
+        temp = image[i]
         j = 0
         while j < x:
-            # imageTemp[i, ((y-1) - j)] = np.array(temp[j])
-            imageTemp[3,3] = 3
+            imageTemp[j, i] = temp[j]
             j += 1
         i += 1
 
-    print(f"\033[1;32mThe shape of image is: {imageTemp.shape}\033[1;37m")
+    print(f"\033[1;32mNew shape after Transpose (Rotate): \
+{imageTemp.shape}\033[1;37m")
     print(imageTemp)
     imageRotate = Image.fromarray(imageTemp)
+# **********************************************************
 
-    #**********************************************************
 
-    
+# ******************** Rotate *****************************
+    imageArray = Image.fromarray(image)
+    transposed_image = Image.new("L", (y, x))
+    for Y in range(y):
+        for X in range(x):
+            pixel = imageArray.getpixel((X, Y))
+            transposed_image.putpixel((Y, X), pixel)
+    print(f"\033[1;32mNew shape after Transpose (Rotate_2): \
+{transposed_image.size}\033[1;37m")
+    print(np.array(transposed_image))
+# **********************************************************
+
     imageNB = Image.fromarray(image)
 
     plt.imshow(imageNB, cmap="gray")
@@ -75,14 +99,20 @@ def rotate():
     plt.show()
 
     plt.imshow(imageReverse, cmap="gray")
-    plt.title("Zoomed Image")
+    plt.title("Zoomed Image Reverse")
     plt.axis('on')
     plt.show()
-    
-    plt.imshow(imageRotate , cmap="gray")
-    plt.title("Zoomed Image")
+
+    plt.imshow(imageRotate, cmap="gray")
+    plt.title("Zoomed Image Rotate")
     plt.axis('on')
     plt.show()
+
+    plt.imshow(transposed_image, cmap="gray")
+    plt.title("Zoomed Image transposed_image")
+    plt.axis('on')
+    plt.show()
+
 
 if __name__ == "__main__":
     rotate()
