f4a8f7a03ca68a57be7f4e6fa3432bf4ebf843a7
Pascal Wolff <pwolff@k0r4p14.42mulhouse.fr>
2024-03-26 07:40:26 +0100

    240326 02-00

---
+++ Python_for_data_science/Python_2_DataTable/ex00/load_csv.py
@@ -0,0 +1,52 @@
+# ****************************************************************************#
+#                                                                             #
+#                                                         :::      ::::::::   #
+#    load_csv.py                                        :+:      :+:    :+:   #
+#                                                     +:+ +:+         +:+     #
+#    By: pwolff <pwolff@student.42.fr>              +#+  +:+       +#+        #
+#                                                 +#+#+#+#+#+   +#+           #
+#    Created: 2024/03/26 07:30:01 by pwolff            #+#    #+#             #
+#    Updated: 2024/03/26 07:30:17 by pwolff           ###   ########.fr       #
+#                                                                             #
+# ****************************************************************************#
+
+
+"""
+python3 -m venv env
+source env/bin/activate
+python3 start.py
+
+or :
+pip install numpy
+pip install Pillow
+pip install matplotlib
+
+    lire la doc :
+    python3
+    import load_image
+    help(load_image)
+
+    from load_image import ft_load
+    help(ft_load)
+
+Chemin pour flack8 :
+export PATH=$PATH:/home/pwolff/.local/bin
+"""
+
+
+# def load(path: str) -> Dataset: (You have to adapt the type of return according to your library)
+def load(path: str) -> Dataset: (You have to adapt the type of return according to your library)
+    pass
+
+
+
+
+
+def main():
+    """
+    """
+    print("\033[1;34mYou want to go the .tester :)\033[1;37m")
+
+
+if __name__ == "__main__":
+    main()
